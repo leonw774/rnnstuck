d23ff5d4fb779bb5cc3f7915ac50e600d65950dd
leonw774 <33317554+leonw774@users.noreply.github.com>
2020-08-08 19:57:53 +0800

    Update rnnstuck.js

--- rnnstuck.js
+++ rnnstuck.js
@@ -1,19 +1,25 @@
 const vocabSize = WORD_INDEX.length;
 const seedSize = SEED_INDEX.length;
+const ZERO_VECTOR = new Array(VECTOR_INDEX[0].length).fill(0.0)
 var model_loaded = false;
+var max_timestep = null;
 model = null;
+
 
 async function load_model() {
   var gen_btn = document.getElementById("gen-btn"),
       load_btn = document.getElementById("load-btn"),
       out_div = document.getElementById("output-div");
   load_btn.disabled = true;
-  out_div.innerText = "........正在載入model........"
-  model = await tf.loadLayersModel('/rnnstuck/jsmodel/model.json');
+  out_div.innerText = "........正在載入model........";
+  //model = await tf.loadLayersModel('./jsmodel/model.json');
+  model = await tf.loadLayersModel('https://leonw774.github.io/rnnstuck/jsmodel/model.json');
+  max_timestep = model.layers[0].inputSpec[0].shape[1];
   model_loaded = true;
   out_div.innerText = "model載入完成。";
   gen_btn.disabled = false;
   load_btn.style.display = "none";
+  return;
 };
 
 function multinomial(probs) {
@@ -44,19 +50,30 @@ function sample(prediction, temperature = 1.0) {
 
 function sentence2vecs(sentence) {
   var result = [];
-  for (var w in sentence) {
-    for (var i in WORD_INDEX) {
-      if (WORD_INDEX[i] == sentence[w]) {
+  if (max_timestep != null) {
+    if (sentence.length > max_timestep) {
+      sentence = sentence.slice(sentence.length-max_timestep);
+    }
+  }
+  for (var n = 0; n < max_timestep; n++) {
+    if (n >= sentence.length) {
+      result.push(ZERO_VECTOR);
+    }
+    else {
+      var w = sentence[n];
+      var i = WORD_INDEX.findIndex(element => element == sentence[w])
+      if (i != -1) {
         result.push(VECTOR_INDEX[i]);
-        break;
+      }
+      else {
+        result.push(ZERO_VECTOR);
       }
     }
   }
   return [result];
 };
 
-async function generate()
-{
+async function generate() {
   if (!model_loaded) return;
   
   var gen_btn = document.getElementById("gen-btn"),
@@ -70,12 +87,14 @@ async function generate()
   var next_word = "", last_word = "";
   for (var i = 0; i < 60; i++) {
     tf.tidy(() => {
-        var y_data = Array.from(model.predict(tf.tensor(sentence2vecs(output_sentence))).dataSync());
+        var vector = sentence2vecs(output_sentence);
+        console.log(vector.length);
+        var y_data = Array.from(model.predict(tf.tensor(vector)).dataSync());
         next_word = WORD_INDEX[sample(y_data, 0.75)];
         y_data = [];
     });
     if (next_word == "ê") break;
-    if (last_word == "\n" && next_word == "\n") continue;
+    //if (last_word == "\n" && next_word == "\n") continue;
     output_sentence.push(next_word);
     last_word = next_word;
   }
