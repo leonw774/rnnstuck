53d7965319f4498cb05a72b36b55a238efbf356e
leonw774 <33317554+leonw774@users.noreply.github.com>
2020-08-08 08:27:47 +0800

    update 2020-08-08

---
+++ make_indexjs.py
@@ -0,0 +1,60 @@
+import re
+import glob
+from gensim.models import word2vec
+
+W2V_BY_EACH_WORD = True
+
+if W2V_BY_EACH_WORD :
+    model = word2vec.Word2Vec.load("./models/myword2vec_by_word.model")
+else :
+    model = word2vec.Word2Vec.load("./models/myword2vec_by_char.model")
+    
+### VOCAB INDEX ###
+index_out = open("wordindex.js", "w+", encoding = 'utf-8-sig')
+index_out.write("var WORD_INDEX = [")
+for i in range(model.wv.syn0.shape[0]) :
+    word = model.wv.index2word[i];
+    if word == '\\' or word == '\"' or word == '\/' :
+        word = '\\' + word
+    if word == '\n' :
+        word = '\\n'
+    if i == 0 :
+        index_out.write("\"" + word + "\"")
+    else :
+        index_out.write(", \"" + word + "\"")
+index_out.write("];")
+print("wordindex done")
+
+### VECTOR INDEX ###
+index_out = open("vectorindex.js", "w+", encoding = 'utf-8-sig')
+index_out.write("var VECTOR_INDEX = [")
+for i in range(model.wv.syn0.shape[0]) :
+    word = model.wv.index2word[i];
+    if i == 0 :
+        index_out.write("[" + ",".join(str(x) for x in model.wv[word]) + "]")
+    else :
+        index_out.write(", " + "[" + ",".join([str(x) for x in model.wv[word]]) + "]")
+index_out.write("];")
+print("vectorindex done")
+
+### SEED ###
+seed_out = open("seedindex.js", "w+", encoding = 'utf-8-sig')
+cut_posts_paths = glob.glob(r"cut_posts/*.txt")
+
+seed_out.write("var SEED_INDEX = [")
+for i, path in enumerate(cut_posts_paths) :
+    line = open(path, 'r', encoding = 'utf-8-sig').readline()
+    line = (re.sub("= = >", "==>", (re.sub("= = = = = = >", "======>", line))))
+    if W2V_BY_EACH_WORD :
+        line = line.split()
+    if len(line) > 0 :
+        word = line[0]
+        if word == '\\' or word == '\"' or word == '\/' :
+            word = '\\' + word
+        if word == '\n' :
+            word = '\\n'
+        if i == 0 :
+            seed_out.write("\"" + word + "\"")
+        else :
+            seed_out.write(", " + "\"" + word + "\"")
+seed_out.write("];")
